d4473ca2a97a461397c64de9c8cc0563799623a6
Rishit121 <rishitlathia29@gmail.com>
2026-04-02 00:39:26 +0530

    Update: added Vectorstore & LLM

--- app.py
+++ app.py
@@ -16,3 +16,33 @@ st.title("Orion AI — Your Personal Study Assistant")
 
 @st.cache_resource
 def load_qa_chain():
+
+  # embeddings + vectorstore
+    embeddings = AzureOpenAIEmbeddings(
+        azure_deployment=os.getenv("AZURE_OPENAI_EMBEDDING_DEPLOYMENT"),
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    )
+
+    db = FAISS.load_local("vectorstore", embeddings, allow_dangerous_deserialization=True)
+
+    # LLM
+    llm = AzureChatOpenAI(
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        azure_deployment=os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT"),
+        openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        temperature=0
+    )
+
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+    return ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=db.as_retriever(),
+        memory=memory
+    )
+
+qa_chain = load_qa_chain()
+
